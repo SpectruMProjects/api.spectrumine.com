3e475b56f55899c161ab24447c6a2e02988b9b25
Савелій Савёнак <1986developer@gmail.com>
2023-03-02 13:29:04 +0300

    Hardcore Service Started

---
+++ SpectruMineAPI/Controllers/HardcoreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpectruMineAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HardcoreController : ControllerBase
+    {
+
+    }
+}
