3a599d718ff9780b0a122d004f0c4bd9fc5c79b1
Gleb <M20XV@yandex.ru>
2023-03-30 12:13:32 +0300

    DTO update

--- SpectruMineAPI/Controllers/AuthController.cs
+++ SpectruMineAPI/Controllers/AuthController.cs
@@ -5,138 +5,138 @@ using SpectruMineAPI.Services.Auth;
 
 namespace SpectruMineAPI.Controllers
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class AuthController : ControllerBase
+  [Route("[controller]")]
+  [ApiController]
+  public class AuthController : ControllerBase
+  {
+    private AuthService authService;
+    public AuthController(AuthService authService) => this.authService = authService;
+    [HttpGet("Users")]
+    public async Task<ActionResult<UsersResponse>> GetUsers()
     {
-        private AuthService authService;
-        public AuthController(AuthService authService) => this.authService = authService;
-        [HttpGet("Users")]
-        public async Task<ActionResult<UsersResponse>> GetUsers()
-        {
-            List<Models.User> users = await authService.GetUsers();
-            return new UsersResponse(users);
-        }
-        [HttpPost("Reg")]
-        public async Task<IActionResult> CreateUser(RegisterQuery query)
-        {
-            var status = await authService.CreateAccount(query.Username, query.Password, query.Email);
-            if (status != AuthService.Errors.Success)
-            {
-                switch (status)
-                {
-                    case AuthService.Errors.RegexNotMatch:
-                        return BadRequest(new Models.Error(status.ToString(), "InvalidData"));
-                    case AuthService.Errors.Conflict:
-                        return Conflict(new Models.Error(status.ToString(), "AccountExists"));
-                }
-            }
-            return Ok();
-        }
-        [HttpPost("Tokens")]
-        public async Task<ActionResult<AuthResponse>> AuthentificateUser(AuthQuery query)
-        {
-            var status = await authService.CheckUser(query.Username, query.Password);
-            switch (status)
-            {
-                case AuthService.Errors.InvalidPassword:
-                    return Unauthorized(new Models.Error(status.ToString(), "WrongPassword"));
-                case AuthService.Errors.UserNotFound:
-                    return Unauthorized(new Models.Error(status.ToString(), "UserNotExist"));
-                case AuthService.Errors.AccountDisabled:
-                    return Unauthorized(new Models.Error(status.ToString(), "CheckMail"));
-            }
-            var auth = await authService.GenerateTokens(query.Username);
-            return new AuthResponse(auth.AccessToken, auth.RefreshToken.Token);
-        }
-        [HttpGet("CheckToken")]
-        [Authorize]
-        public ActionResult CheckToken()
-        {
-            return Ok(User.Identity!.Name);
-        }
-        /// <summary>
-        /// Update refreshToken method
-        /// </summary>
-        /// <param name="query"></param>
-        /// <returns></returns>
-        [HttpPost("UpdateToken")]
-        public async Task<ActionResult<UpdateResponse>> UpdateToken(UpdateQuery query)
-        {
-            var status = await authService.CheckToken(query.RefreshToken);
-            switch (status)
-            {
-                case AuthService.Errors.UserNotFound:
-                    return Unauthorized(new Models.Error(status.ToString(), "TokenNotExist"));
-                case AuthService.Errors.TokenExpire:
-                    return Unauthorized(new Models.Error(status.ToString(), "TokenExipred"));
-            }
-            var response = await authService.UpdateTokens(query.RefreshToken);
-            return new UpdateResponse(response.AccessToken, response.RefreshToken.Token);
-        }
-        /// <summary>
-        /// Logout other tokens method
-        /// </summary>
-        [HttpPost("ReloadTokens")]
-        public async Task<ActionResult<UpdateResponse>> ReloadTokens(UpdateQuery query)
-        {
-            var status = await authService.CheckToken(query.RefreshToken);
-            switch (status)
-            {
-                case AuthService.Errors.UserNotFound:
-                    return Unauthorized(new Models.Error(status.ToString(), "TokenNotExist"));
-                case AuthService.Errors.TokenExpire:
-                    return Unauthorized(new Models.Error(status.ToString(), "TokenExipred"));
-            }
-            var response = await authService.UpdateTokens(query.RefreshToken);
-            return new UpdateResponse(response.AccessToken, response.RefreshToken.Token);
-        }
-
-        /// <summary>
-        /// Logout current token method
-        /// </summary>
-        [HttpPost("Logout")]
-        public async Task<ActionResult> Logout(UpdateQuery query)
-        {
-            var status = await authService.CheckToken(query.RefreshToken);
-            switch (status)
-            {
-                case AuthService.Errors.UserNotFound:
-                    return Unauthorized(new Models.Error(status.ToString(), "TokenNotExist"));
-                case AuthService.Errors.TokenExpire:
-                    return Unauthorized(new Models.Error(status.ToString(), "TokenExipred"));
-            }
-            authService.RemoveToken(query.RefreshToken);
-            return Ok();
-        }
-        [HttpPost("ResetPassword")]
-        public async Task<ActionResult> ResetPass(ResetPassQuery query)
-        {
-            var status = await authService.UpdatePassword(query.Email, query.NewPassword);
-            switch (status)
-            {
-                case AuthService.Errors.UserNotFound:
-                    return NotFound(new Models.Error(status.ToString(), "IncorrectMail"));
-                case AuthService.Errors.RegexNotMatch:
-                    return BadRequest(new Models.Error(status.ToString(), "IncorrectData"));
-            }
-            return Ok();
-        }
-        [Authorize]
-        [HttpPost("ResetPasswordAuth")]
-        public async Task<ActionResult> ResetPassAuth(ResetPassQueryAuth query)
-        {
-            var userMail = await authService.GetMailByUsername(User.Identity!.Name!);
-            if (userMail == null) return BadRequest(new Models.Error("Null", "Каким образом это вообще могло возникнуть? Ты долбаёб?"));
-            var status = await authService.UpdatePassword(userMail, query.NewPassword);
-            switch (status)
-            {
-                case AuthService.Errors.UserNotFound:
-                    return NotFound(new Models.Error(status.ToString(), "IncorrectMail"));
-                case AuthService.Errors.RegexNotMatch:
-                    return BadRequest(new Models.Error(status.ToString(), "IncorrectData"));
-            }
-            return Ok();
-        }
+      List<Models.User> users = await authService.GetUsers();
+      return new UsersResponse(users.ConvertAll(user => user.Map()));
     }
+    [HttpPost("Reg")]
+    public async Task<IActionResult> CreateUser(RegisterQuery query)
+    {
+      var status = await authService.CreateAccount(query.Username, query.Password, query.Email);
+      if (status != AuthService.Errors.Success)
+      {
+        switch (status)
+        {
+          case AuthService.Errors.RegexNotMatch:
+            return BadRequest(new Models.Error(status.ToString(), "InvalidData"));
+          case AuthService.Errors.Conflict:
+            return Conflict(new Models.Error(status.ToString(), "AccountExists"));
+        }
+      }
+      return Ok();
+    }
+    [HttpPost("Tokens")]
+    public async Task<ActionResult<AuthResponse>> AuthentificateUser(AuthQuery query)
+    {
+      var status = await authService.CheckUser(query.Username, query.Password);
+      switch (status)
+      {
+        case AuthService.Errors.InvalidPassword:
+          return Unauthorized(new Models.Error(status.ToString(), "WrongPassword"));
+        case AuthService.Errors.UserNotFound:
+          return Unauthorized(new Models.Error(status.ToString(), "UserNotExist"));
+        case AuthService.Errors.AccountDisabled:
+          return Unauthorized(new Models.Error(status.ToString(), "CheckMail"));
+      }
+      var auth = await authService.GenerateTokens(query.Username);
+      return new AuthResponse(auth.AccessToken, auth.RefreshToken.Token);
+    }
+    [HttpGet("CheckToken")]
+    [Authorize]
+    public ActionResult CheckToken()
+    {
+      return Ok(User.Identity!.Name);
+    }
+    /// <summary>
+    /// Update refreshToken method
+    /// </summary>
+    /// <param name="query"></param>
+    /// <returns></returns>
+    [HttpPost("UpdateToken")]
+    public async Task<ActionResult<UpdateResponse>> UpdateToken(UpdateQuery query)
+    {
+      var status = await authService.CheckToken(query.RefreshToken);
+      switch (status)
+      {
+        case AuthService.Errors.UserNotFound:
+          return Unauthorized(new Models.Error(status.ToString(), "TokenNotExist"));
+        case AuthService.Errors.TokenExpire:
+          return Unauthorized(new Models.Error(status.ToString(), "TokenExipred"));
+      }
+      var response = await authService.UpdateTokens(query.RefreshToken);
+      return new UpdateResponse(response.AccessToken, response.RefreshToken.Token);
+    }
+    /// <summary>
+    /// Logout other tokens method
+    /// </summary>
+    [HttpPost("ReloadTokens")]
+    public async Task<ActionResult<UpdateResponse>> ReloadTokens(UpdateQuery query)
+    {
+      var status = await authService.CheckToken(query.RefreshToken);
+      switch (status)
+      {
+        case AuthService.Errors.UserNotFound:
+          return Unauthorized(new Models.Error(status.ToString(), "TokenNotExist"));
+        case AuthService.Errors.TokenExpire:
+          return Unauthorized(new Models.Error(status.ToString(), "TokenExipred"));
+      }
+      var response = await authService.UpdateTokens(query.RefreshToken);
+      return new UpdateResponse(response.AccessToken, response.RefreshToken.Token);
+    }
+
+    /// <summary>
+    /// Logout current token method
+    /// </summary>
+    [HttpPost("Logout")]
+    public async Task<ActionResult> Logout(UpdateQuery query)
+    {
+      var status = await authService.CheckToken(query.RefreshToken);
+      switch (status)
+      {
+        case AuthService.Errors.UserNotFound:
+          return Unauthorized(new Models.Error(status.ToString(), "TokenNotExist"));
+        case AuthService.Errors.TokenExpire:
+          return Unauthorized(new Models.Error(status.ToString(), "TokenExipred"));
+      }
+      authService.RemoveToken(query.RefreshToken);
+      return Ok();
+    }
+    [HttpPost("ResetPassword")]
+    public async Task<ActionResult> ResetPass(ResetPassQuery query)
+    {
+      var status = await authService.UpdatePassword(query.Email, query.NewPassword);
+      switch (status)
+      {
+        case AuthService.Errors.UserNotFound:
+          return NotFound(new Models.Error(status.ToString(), "IncorrectMail"));
+        case AuthService.Errors.RegexNotMatch:
+          return BadRequest(new Models.Error(status.ToString(), "IncorrectData"));
+      }
+      return Ok();
+    }
+    [Authorize]
+    [HttpPost("ResetPasswordAuth")]
+    public async Task<ActionResult> ResetPassAuth(ResetPassQueryAuth query)
+    {
+      var userMail = await authService.GetMailByUsername(User.Identity!.Name!);
+      if (userMail == null) return BadRequest(new Models.Error("Null", "Каким образом это вообще могло возникнуть? Ты долбаёб?"));
+      var status = await authService.UpdatePassword(userMail, query.NewPassword);
+      switch (status)
+      {
+        case AuthService.Errors.UserNotFound:
+          return NotFound(new Models.Error(status.ToString(), "IncorrectMail"));
+        case AuthService.Errors.RegexNotMatch:
+          return BadRequest(new Models.Error(status.ToString(), "IncorrectData"));
+      }
+      return Ok();
+    }
+  }
 }
--- SpectruMineAPI/Controllers/AuthDTO.cs
+++ SpectruMineAPI/Controllers/AuthDTO.cs
@@ -2,15 +2,28 @@
 
 namespace SpectruMineAPI.Controllers
 {
-    namespace AuthDTO
+  namespace AuthDTO
+  {
+    public record UsersResponse(List<Public.User> Users);
+    public record RegisterQuery(string Username, string Password, string Email);
+    public record AuthQuery(string Username, string Password);
+    public record AuthResponse(string AccessToken, string RefreshToken);
+    public record UpdateQuery(string RefreshToken);
+    public record UpdateResponse(string AccessToken, string RefreshToken);
+    public record ResetPassQuery(string Email, string NewPassword);
+    public record ResetPassQueryAuth(string NewPassword);
+
+    namespace Public
     {
-        public record UsersResponse(List<User> Users);
-        public record RegisterQuery(string Username, string Password, string Email);
-        public record AuthQuery(string Username, string Password);
-        public record AuthResponse(string AccessToken, string RefreshToken);
-        public record UpdateQuery(string RefreshToken);
-        public record UpdateResponse(string AccessToken, string RefreshToken);
-        public record ResetPassQuery(string Email, string NewPassword);
-        public record ResetPassQueryAuth(string NewPassword);
+      public record User(string Id, string Username);
     }
+  }
+
+  static class Mapper
+  {
+    public static AuthDTO.Public.User Map(this User User)
+    {
+      return new(User.Id, User.Username);
+    }
+  }
 }
