61fb5559430b3b05e74cd78929692ad1e6fe9b36
Gleb <M20XV@yandex.ru>
2023-03-30 11:54:30 +0300

    fix

--- SpectruMineAPI/Services/Auth/AuthService.cs
+++ SpectruMineAPI/Services/Auth/AuthService.cs
@@ -9,199 +9,200 @@ using System.Text.RegularExpressions;
 
 namespace SpectruMineAPI.Services.Auth
 {
-    public class AuthService
+  public class AuthService
+  {
+    private UserCRUD Users;
+    private Mail.MailSenderService MailService;
+    public AuthService(UserCRUD users, Mail.MailSenderService mailService)
     {
-        private UserCRUD Users;
-        private Mail.MailSenderService MailService;
-        public AuthService(UserCRUD users, Mail.MailSenderService mailService)
+      Users = users;
+      MailService = mailService;
+    }
+    public async Task<Errors> CreateAccount(string Username, string Password, string Email)
+    {
+      var regexUsername = new Regex(@"^[a-zA-Z0-9_]{3,16}$");
+      var regexPassword = new Regex(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,32}$");
+      var regexMail = new Regex("^[a-zA-Z0-9.!#$%&’*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$");
+      //Секция проверки данных
+      if (!regexUsername.IsMatch(Username) || !regexPassword.IsMatch(Password) || !regexMail.IsMatch(Email))
+      {
+        return Errors.RegexNotMatch;
+      }
+      //Секция проверки существования другого аккаунта
+      var user = await Users.GetAsync(x => x._username == Username.ToLower());
+      if (user != null)
+      {
+        if (!user.Verified)
         {
-            Users = users;
-            MailService = mailService;
+          await Users.RemoveAsync(user.Id);
         }
-        public async Task<Errors> CreateAccount(string Username, string Password, string Email)
+        else return Errors.Conflict;
+      }
+      var code = Crypto.CalculateMD5(DateTime.UtcNow.ToString());
+      //Создание аккаунта
+
+      if (AuthOptions.UseMail)
+      {
+        await Users.CreateAsync(new()
         {
-            var regexUsername = new Regex(@"^[a-zA-Z0-9_]{3,16}$");
-            var regexPassword = new Regex(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,32}$");
-            var regexMail = new Regex("^[a-zA-Z0-9.!#$%&’*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$");
-            //Секция проверки данных
-            if (!regexUsername.IsMatch(Username) || !regexPassword.IsMatch(Password) || !regexMail.IsMatch(Email))
-            {
-                return Errors.RegexNotMatch;
-            }
-            //Секция проверки существования другого аккаунта
-            var user = await Users.GetAsync(x => x._username == Username.ToLower());
-            if (user != null)
-            {
-                if (!user.Verified)
-                {
-                    await Users.RemoveAsync(user.Id);
-                }
-                else return Errors.Conflict;
-            }
-            var code = Crypto.CalculateMD5(DateTime.UtcNow.ToString());
-            //Создание аккаунта
-            
-            if (AuthOptions.UseMail)
-            {
-                await Users.CreateAsync(new()
-                {
-                    Username = Username,
-                    _username = Username.ToLower(),
-                    Password = Crypto.CalculateSHA256(Password),
-                    Email = Email,
-                    MailCodes = new() {
+          Username = Username,
+          _username = Username.ToLower(),
+          Password = Crypto.CalculateSHA256(Password),
+          Email = Email,
+          MailCodes = new() {
                     new()
                     {
                         Code = code,
                         ExpireAt = DateTime.UtcNow.AddMinutes(5)
                     }
                 }
-                });
-                //Отправка на почту кода регистрации
-                MailService.SendMessageActivate(Email, code);
-            }
-            else
-            {
-                await Users.CreateAsync(new()
-                {
-                    Username = Username,
-                    _username = Username.ToLower(),
-                    Password = Crypto.CalculateSHA256(Password),
-                    Email = Email,
-                    Verified = true
-                });
-            }
-            return Errors.Success;
-        }
-        public async Task<Errors> CheckUser(string Username, string Password)
+        });
+        //Отправка на почту кода регистрации
+        MailService.SendMessageActivate(Email, code);
+      }
+      else
+      {
+        Console.WriteLine("Activation code is: " + code);
+        await Users.CreateAsync(new()
         {
-            var user = await Users.GetAsync(x => x._username == Username.ToLower());
-            if (user == null) return Errors.UserNotFound;
-            if (user.Password != Crypto.CalculateSHA256(Password)) return Errors.InvalidPassword;
-            if (!user.Verified) return Errors.AccountDisabled;
-            return Errors.Success;
-        }
-        public async Task<Errors> CheckToken(string refreshToken)
-        {
-            var userList = await Users.GetAsync();
-            var user = userList.FirstOrDefault(x => x.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken) != null);
-            if (user == null) return Errors.UserNotFound;
-            var Token = user.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken);
-            if (Token!.ExpireAt < DateTime.UtcNow)
-            {
-                user.RefreshTokens.Remove(Token);
-                await Users.UpdateAsync(user.Id, user);
-                return Errors.TokenExpire;
-            }
-            return Errors.Success;
-        }
-        public async Task<Tokens> GenerateTokens(string Username)
-        {
-            var user = await Users.GetAsync(x => x._username == Username.ToLower());
-            if (user == null) throw new ArgumentNullException($"{nameof(user)} returned null");
-            var refreshToken = new RefreshToken()
-            {
-                Token = Crypto.CalculateSHA256(DateTime.UtcNow.ToString()),
-                ExpireAt = DateTime.UtcNow.AddDays(30)
-            };
-            var accessToken = Crypto.GetAccessToken(user._username);
-            user.RefreshTokens.Add(refreshToken);
-            await Users.UpdateAsync(user.Id, user);
-            return new(accessToken, refreshToken);
-        }
-        public async Task<Tokens> UpdateTokens(string refreshToken)
-        {
-            var userList = await Users.GetAsync();
-            var user = userList.FirstOrDefault(x => x.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken) != null);
-            if (user == null) throw new ArgumentNullException($"{nameof(user)} returned null");
-            var newToken = new RefreshToken()
-            {
-                Token = Crypto.CalculateSHA256(DateTime.UtcNow.ToString()),
-                ExpireAt = DateTime.UtcNow.AddDays(30)
-            };
-            var accessToken = Crypto.GetAccessToken(user._username);
-            user.RefreshTokens.Remove(user.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken)!);
-            user.RefreshTokens.Add(newToken);
-            await Users.UpdateAsync(user.Id, user);
-            return new(accessToken, newToken);
-        }
-        public async Task<Tokens> RemoveTokens(string refreshToken)
-        {
-            var userList = await Users.GetAsync();
-            var user = userList.FirstOrDefault(x => x.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken) != null);
-            if (user == null) throw new ArgumentNullException($"{nameof(user)} returned null");
-            user.RefreshTokens.Clear();
-            var newToken = new RefreshToken()
-            {
-                Token = Crypto.CalculateSHA256(DateTime.UtcNow.ToString()),
-                ExpireAt = DateTime.UtcNow.AddDays(30)
-            };
-            var accessToken = Crypto.GetAccessToken(user._username);
-            user.RefreshTokens.Remove(user.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken)!);
-            user.RefreshTokens.Add(newToken);
-            await Users.UpdateAsync(user.Id, user);
-            return new(accessToken, newToken);
-        }
-        public async void RemoveToken(string refreshToken)
-        {
-            var userList = await Users.GetAsync();
-            var user = userList.FirstOrDefault(x => x.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken) != null);
-            if (user == null) throw new ArgumentNullException($"{nameof(user)} returned null");
-            user.RefreshTokens.Remove(user.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken)!);
-            await Users.UpdateAsync(user.Id, user);
-        }
-        public async Task<Errors> UpdatePassword(string email, string password)
-        {
-            var regexMail = new Regex("^[a-zA-Z0-9.!#$%&’*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$");
-            var regexPassword = new Regex(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,32}$");
-            var user = await Users.GetAsync(x => x.Email == email);
-            if (user == null) return Errors.UserNotFound;
-            if (!regexPassword.IsMatch(password) || !regexMail.IsMatch(email)) return Errors.RegexNotMatch;
-            user.NewPassword = Crypto.CalculateSHA256(password);
-            var code = Crypto.CalculateMD5(DateTime.UtcNow.ToString());
-            user.MailCodes.Add(new()
-            {
-                Code = code,
-                ExpireAt = DateTime.UtcNow.AddMinutes(5),
-                isRestore = true
-            });
-            await Users.UpdateAsync(user.Id, user);
-            MailService.SendMessageRestore(email, code);
-            return Errors.Success;
-        }
-        public async Task<string?> GetMailByUsername(string username)
-        {
-            var user = await Users.GetAsync(x => x._username == username);//Предусматривается что тут username уже ToLower
-            if (user == null) return null;
-            return user.Email;
-        }
-        public async Task<List<User>> GetUsers()
-        {
-            return await Users.GetAsync();
-        }
-        public enum Errors { RegexNotMatch, Success, Conflict, UserNotFound, InvalidPassword, TokenExpire, AccountDisabled }
-        public record Tokens(string AccessToken, RefreshToken RefreshToken);
+          Username = Username,
+          _username = Username.ToLower(),
+          Password = Crypto.CalculateSHA256(Password),
+          Email = Email,
+          Verified = false
+        });
+      }
+      return Errors.Success;
     }
-    static class Crypto
+    public async Task<Errors> CheckUser(string Username, string Password)
     {
-        public static string CalculateSHA256(string data)
-        {
-            return Convert.ToHexString(SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(data))).ToLower();
-        }
-        public static string CalculateMD5(string data)
-        {
-            return Convert.ToHexString(MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(data))).ToLower();
-        }
-        public static string GetAccessToken(string username)
-        {
-            var claims = new List<Claim> { new Claim(ClaimTypes.Name, username) };
-            var jwt = new JwtSecurityToken(
-                    issuer: AuthOptions.ISSUER,
-                    audience: AuthOptions.AUDIENCE,
-                    claims: claims,
-                    expires: DateTime.UtcNow, //Действует 5 минут
-                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
-            return new JwtSecurityTokenHandler().WriteToken(jwt);
-        }
+      var user = await Users.GetAsync(x => x._username == Username.ToLower());
+      if (user == null) return Errors.UserNotFound;
+      if (user.Password != Crypto.CalculateSHA256(Password)) return Errors.InvalidPassword;
+      if (!user.Verified) return Errors.AccountDisabled;
+      return Errors.Success;
     }
+    public async Task<Errors> CheckToken(string refreshToken)
+    {
+      var userList = await Users.GetAsync();
+      var user = userList.FirstOrDefault(x => x.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken) != null);
+      if (user == null) return Errors.UserNotFound;
+      var Token = user.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken);
+      if (Token!.ExpireAt < DateTime.UtcNow)
+      {
+        user.RefreshTokens.Remove(Token);
+        await Users.UpdateAsync(user.Id, user);
+        return Errors.TokenExpire;
+      }
+      return Errors.Success;
+    }
+    public async Task<Tokens> GenerateTokens(string Username)
+    {
+      var user = await Users.GetAsync(x => x._username == Username.ToLower());
+      if (user == null) throw new ArgumentNullException($"{nameof(user)} returned null");
+      var refreshToken = new RefreshToken()
+      {
+        Token = Crypto.CalculateSHA256(DateTime.UtcNow.ToString()),
+        ExpireAt = DateTime.UtcNow.AddDays(30)
+      };
+      var accessToken = Crypto.GetAccessToken(user._username);
+      user.RefreshTokens.Add(refreshToken);
+      await Users.UpdateAsync(user.Id, user);
+      return new(accessToken, refreshToken);
+    }
+    public async Task<Tokens> UpdateTokens(string refreshToken)
+    {
+      var userList = await Users.GetAsync();
+      var user = userList.FirstOrDefault(x => x.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken) != null);
+      if (user == null) throw new ArgumentNullException($"{nameof(user)} returned null");
+      var newToken = new RefreshToken()
+      {
+        Token = Crypto.CalculateSHA256(DateTime.UtcNow.ToString()),
+        ExpireAt = DateTime.UtcNow.AddDays(30)
+      };
+      var accessToken = Crypto.GetAccessToken(user._username);
+      user.RefreshTokens.Remove(user.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken)!);
+      user.RefreshTokens.Add(newToken);
+      await Users.UpdateAsync(user.Id, user);
+      return new(accessToken, newToken);
+    }
+    public async Task<Tokens> RemoveTokens(string refreshToken)
+    {
+      var userList = await Users.GetAsync();
+      var user = userList.FirstOrDefault(x => x.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken) != null);
+      if (user == null) throw new ArgumentNullException($"{nameof(user)} returned null");
+      user.RefreshTokens.Clear();
+      var newToken = new RefreshToken()
+      {
+        Token = Crypto.CalculateSHA256(DateTime.UtcNow.ToString()),
+        ExpireAt = DateTime.UtcNow.AddDays(30)
+      };
+      var accessToken = Crypto.GetAccessToken(user._username);
+      user.RefreshTokens.Remove(user.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken)!);
+      user.RefreshTokens.Add(newToken);
+      await Users.UpdateAsync(user.Id, user);
+      return new(accessToken, newToken);
+    }
+    public async void RemoveToken(string refreshToken)
+    {
+      var userList = await Users.GetAsync();
+      var user = userList.FirstOrDefault(x => x.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken) != null);
+      if (user == null) throw new ArgumentNullException($"{nameof(user)} returned null");
+      user.RefreshTokens.Remove(user.RefreshTokens.FirstOrDefault(x => x.Token == refreshToken)!);
+      await Users.UpdateAsync(user.Id, user);
+    }
+    public async Task<Errors> UpdatePassword(string email, string password)
+    {
+      var regexMail = new Regex("^[a-zA-Z0-9.!#$%&’*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$");
+      var regexPassword = new Regex(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,32}$");
+      var user = await Users.GetAsync(x => x.Email == email);
+      if (user == null) return Errors.UserNotFound;
+      if (!regexPassword.IsMatch(password) || !regexMail.IsMatch(email)) return Errors.RegexNotMatch;
+      user.NewPassword = Crypto.CalculateSHA256(password);
+      var code = Crypto.CalculateMD5(DateTime.UtcNow.ToString());
+      user.MailCodes.Add(new()
+      {
+        Code = code,
+        ExpireAt = DateTime.UtcNow.AddMinutes(5),
+        isRestore = true
+      });
+      await Users.UpdateAsync(user.Id, user);
+      MailService.SendMessageRestore(email, code);
+      return Errors.Success;
+    }
+    public async Task<string?> GetMailByUsername(string username)
+    {
+      var user = await Users.GetAsync(x => x._username == username);//Предусматривается что тут username уже ToLower
+      if (user == null) return null;
+      return user.Email;
+    }
+    public async Task<List<User>> GetUsers()
+    {
+      return await Users.GetAsync();
+    }
+    public enum Errors { RegexNotMatch, Success, Conflict, UserNotFound, InvalidPassword, TokenExpire, AccountDisabled }
+    public record Tokens(string AccessToken, RefreshToken RefreshToken);
+  }
+  static class Crypto
+  {
+    public static string CalculateSHA256(string data)
+    {
+      return Convert.ToHexString(SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(data))).ToLower();
+    }
+    public static string CalculateMD5(string data)
+    {
+      return Convert.ToHexString(MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(data))).ToLower();
+    }
+    public static string GetAccessToken(string username)
+    {
+      var claims = new List<Claim> { new Claim(ClaimTypes.Name, username) };
+      var jwt = new JwtSecurityToken(
+              issuer: AuthOptions.ISSUER,
+              audience: AuthOptions.AUDIENCE,
+              claims: claims,
+              expires: DateTime.UtcNow, //Действует 5 минут
+              signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+      return new JwtSecurityTokenHandler().WriteToken(jwt);
+    }
+  }
 }
