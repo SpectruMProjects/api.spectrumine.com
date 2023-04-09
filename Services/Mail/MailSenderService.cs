233d2bfaeb3ec4369839b4987ff4cdab388bdc90
Gleb <M20XV@yandex.ru>
2023-04-09 18:21:36 +0300

    add fake mail send

--- Services/Mail/MailSenderService.cs
+++ Services/Mail/MailSenderService.cs
@@ -4,52 +4,66 @@ using MimeKit;
 
 namespace SpectruMineAPI.Services.Mail
 {
-    public class MailSenderService
+  public class MailSenderService
+  {
+    private MailData mailData;
+    private ILogger<MailSenderService> Logger;
+
+    public MailSenderService(IOptions<MailData> mailData, ILogger<MailSenderService> logger)
     {
-        private MailData mailData;
-        public MailSenderService(IOptions<MailData> mailData)
-        {
-            this.mailData = mailData.Value;
-        }
-        public async void SendMessageActivate(string subject, string code)
-        {
-            using var emailMessage = new MimeMessage();
-            emailMessage.From.Add(new MailboxAddress("Администрация сайта", mailData.Login));
-            emailMessage.To.Add(new MailboxAddress("", subject));
-            emailMessage.Subject = "helloworld";
-            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-            {
-                Text = code
-            };
-
-            using (var client = new SmtpClient())
-            {
-                await client.ConnectAsync(mailData.SMTPAddr, mailData.SMTPPort, mailData.SSL);
-                await client.AuthenticateAsync(mailData.Login, mailData.Password);
-                await client.SendAsync(emailMessage);
-
-                await client.DisconnectAsync(true);
-            }
-        }
-        public async void SendMessageRestore(string subject, string code)
-        {
-            using var emailMessage = new MimeMessage();
-            emailMessage.From.Add(new MailboxAddress("Администрация сайта", mailData.Login));
-            emailMessage.To.Add(new MailboxAddress("", subject));
-            emailMessage.Subject = "helloworld";
-            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-            {
-                Text = code
-            };
-
-            using (var client = new SmtpClient())
-            {
-                await client.ConnectAsync(mailData.SMTPAddr, mailData.SMTPPort, mailData.SSL);
-                await client.AuthenticateAsync(mailData.Login, mailData.Password);
-                await client.SendAsync(emailMessage);
-
-                await client.DisconnectAsync(true);
-            }
-        }
+      this.mailData = mailData.Value;
+      this.Logger = logger;
     }
+    public async void SendMessageActivate(string subject, string code)
+    {
+      if (!Services.Auth.AuthOptions.UseMail)
+      {
+        Logger.LogInformation($"Отправка кода активации {subject} {code}");
+        return;
+      }
+      using var emailMessage = new MimeMessage();
+      emailMessage.From.Add(new MailboxAddress("Администрация сайта", mailData.Login));
+      emailMessage.To.Add(new MailboxAddress("", subject));
+      emailMessage.Subject = "helloworld";
+      emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+      {
+        Text = code
+      };
+
+      using (var client = new SmtpClient())
+      {
+        await client.ConnectAsync(mailData.SMTPAddr, mailData.SMTPPort, mailData.SSL);
+        await client.AuthenticateAsync(mailData.Login, mailData.Password);
+        await client.SendAsync(emailMessage);
+
+        await client.DisconnectAsync(true);
+      }
+    }
+    public async void SendMessageRestore(string subject, string code)
+    {
+      if (!Services.Auth.AuthOptions.UseMail)
+      {
+        Logger.LogInformation($"Отправка кода активации {subject} {code}");
+        return;
+      }
+
+      using var emailMessage = new MimeMessage();
+      emailMessage.From.Add(new MailboxAddress("Администрация сайта", mailData.Login));
+      emailMessage.To.Add(new MailboxAddress("", subject));
+      emailMessage.Subject = "helloworld";
+      emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+      {
+        Text = code
+      };
+
+      using (var client = new SmtpClient())
+      {
+        await client.ConnectAsync(mailData.SMTPAddr, mailData.SMTPPort, mailData.SSL);
+        await client.AuthenticateAsync(mailData.Login, mailData.Password);
+        await client.SendAsync(emailMessage);
+
+        await client.DisconnectAsync(true);
+      }
+    }
+  }
 }
