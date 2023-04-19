4ec0fa5e094e71c678a04ffc5c24a382564ec97f
Савелій Савёнак <1986developer@gmail.com>
2023-04-19 21:38:39 +0300

    Added payment

---
+++ Services/Payment/PaymentData.cs
@@ -0,0 +1,9 @@
+﻿using Org.BouncyCastle.Asn1.Ocsp;
+
+namespace SpectruMineAPI.Services.Payment
+{
+    public class PaymentData
+    {
+        public string Signature { get; set; } = null!;
+    }
+}
