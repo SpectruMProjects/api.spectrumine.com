a91a92391add824f5e9e17b803892906030c0d3b
Савелій Савёнак <1986developer@gmail.com>
2023-04-28 21:18:54 +0300

    Added HTML Mail HERE (Dev build previous)

--- Services/Mail/MailSenderService.cs
+++ Services/Mail/MailSenderService.cs
@@ -1,6 +1,7 @@
 ﻿using MailKit.Net.Smtp;
 using Microsoft.Extensions.Options;
 using MimeKit;
+using Razor.Templating.Core;
 
 namespace SpectruMineAPI.Services.Mail
 {
@@ -26,7 +27,7 @@ namespace SpectruMineAPI.Services.Mail
             emailMessage.To.Add(new MailboxAddress(subject, subject));
             emailMessage.Subject = "Подтверждение регистрации";
             var body = new BodyBuilder();
-            body.HtmlBody = CreateHtml(
+            body.HtmlBody = await CreateHtml(
                 "Подтверждение регистрации",
                 "Рады приветсвовать вас на нашей платформе. Чтобы подтвердить регистрацию нажмите на кнопку",
                 "https://dev.spectrumine.com/auth/activate-register/" + code
@@ -55,7 +56,7 @@ namespace SpectruMineAPI.Services.Mail
             emailMessage.Subject = "helloworld";
             emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
             {
-                Text = CreateHtml(
+                Text = await CreateHtml(
                 "Смена пароля",
                 "Создан запрос для измения пароля на проекте SpectruMine. Чтобы подтвердить смену пароля нажмите на кнопку",
                 "https://dev.spectrumine.com/auth/activate-change-pass/" + code
@@ -67,29 +68,52 @@ namespace SpectruMineAPI.Services.Mail
                 await client.ConnectAsync(mailData.SMTPAddr, mailData.SMTPPort, mailData.SSL);
                 await client.AuthenticateAsync(mailData.Login, mailData.Password);
                 await client.SendAsync(emailMessage);
-
                 await client.DisconnectAsync(true);
             }
         }
-
-        private string CreateHtml(string title, string text, string link)
+        private async Task<string> CreateHtml(string title, string text, string link)
         {
-            return
-                        //"<includetail>" +
-                        "<style>" +
-                        "h1 {text-align: center;}" +
-                        "button {background-color: #73d13d;padding: 1em 2em;}" +
-                        "button a {font-size: large;color: white;text-decoration: none;}" +
-                        "p {padding: 1em;}" +
-                        ".button-container {display: flex;align-items: center;justify-content: center;}" +
-                      "</style>" +
-                      "<h1 style=\"text-align: center\">" + title + "</h1>" +
-                      "<p>" + text + "</p>" +
-                        //"<div class=\"button-container\">" +
-                        "<button style=\"background-color: #73d13d;padding: 1em 2em;\">" +
-                          "<a style=\"font-size: large;color: white;text-decoration: none;\" href=\""  + link + "\">" + "Подтвердить" + "</a>" +
-                        "</button>";// +
-                      //"</div>" + "</includetail>";
+            return await RazorTemplateEngine.RenderAsync("~/Pages/mail.cshtml", new TemplateData()
+            {
+                title = title,
+                text = text,
+                link = link
+            });
         }
+        //private string CreateHtml(string title, string text, string link)
+        //{
+        //    return $"""
+        //        <h1 style="text-align: center"> {title} на SpectruMine.com</h1>
+        //        <table width=100% border=0 cellspacing=0 cellpadding=0>
+        //            <tr>
+        //                <td align="center">
+        //                    <p style="padding: 1em"> {text} </p>
+        //                </td>
+        //            </tr>
+        //            <tr>
+        //                <td align="center">
+        //                    <button style="background-color: #73d13d;padding: 1em 2em">
+        //                        <a style="font-size: large;color: white;text-decoration: none;" href="{link}"> Подтвердить </a>
+        //                    </button>
+        //                </td>
+        //            </tr>
+        //            <tr>
+        //                <td align="center">
+        //                    <p style="padding: 1em">Если кнопка не работает, перейдите по этой ссылке:
+        //                        <a href="{link}"> {link} </a>
+        //                    </p>
+        //                </td>
+        //            </tr>
+        //            <tr>
+        //                <td align="center">
+        //                    <p style="padding: 1em">
+        //                        Если это сообщение отправлено по ошибке или вы не совершали никаких действий на платформе
+        //                        spectrumine.com - проигнорируйте это письмо
+        //                    </p>
+        //                </td>
+        //            </tr>
+        //        </table>
+        //        """;
+        //}
     }
 }
---
+++ Services/Mail/TemplateData.cs
@@ -0,0 +1,9 @@
+﻿namespace SpectruMineAPI.Services.Mail
+{
+    public class TemplateData
+    {
+        public string title { get; set; } = null!;
+        public string text { get; set; } = null!;
+        public string link { get; set; } = null!;
+    }
+}
