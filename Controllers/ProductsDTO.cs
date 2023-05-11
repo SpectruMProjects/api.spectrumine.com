3caa5a5a770b4f94a09aec367fcade4a3ded7f07
Савелій Савёнак <1986developer@gmail.com>
2023-05-11 10:53:03 +0300

    Release remove

--- Controllers/ProductsDTO.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace SpectruMineAPI.Controllers
-{
-    public record CreateProductDTO(string Name, string Description, string Category, string ImgUrl, string ObjUrl, string MatUrl, float Price);
-}
